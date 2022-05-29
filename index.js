d6d2faa293b8cdb7b47130714325d31437d28c27
BAGINDA_ECHASIO <baginda.echasio@gmail.com>
2022-05-29 19:48:27 +0700

    input data user #3

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+
+//define favourite color & favourite estaurant monica (1)
+const color1 = Array.from(
+    new Set (["Yellow","Pink","White","Purple"])
+);
+const resto1 = Array.from(
+    new Set (["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"])
+);
+
+//define favourite color & favourite estaurant wendy (2)
+const color2 = Array.from(
+    new Set (["Blue","Black","Grey"])
+);
+const resto2 = Array.from(
+    new Set (["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"])
+);
+
 const firstUser = {
     name: "Monica",
     gender: "Female",
     age: "17",
     email: "monica@dingdong.com",
-    favoriteColor: new Set(["Yellow","Pink","White","Purple"]),
+    favoriteColor: color1,
     isHavePet: true,
     education:[{
         name: "SD 01",
@@ -21,14 +38,14 @@ const firstUser = {
         city: "Tangerang",
         graduate: "0"
     }],
-    favoriteRestaurant: new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]),
+    favoriteRestaurant: resto1,
 };
 const secondUser = {
     name: "Wendy",
     gender: "Male",
     age: "23",
     email: "wendya@dingdong.com",
-    favoriteColor: new Set(["Blue","Black","Grey"]),
+    favoriteColor: color2,
     isHavePet: false,
     education:[{
         name: "SD 02",
@@ -47,7 +64,7 @@ const secondUser = {
         city: "Tangerang",
         graduate: "0"
     }],
-    favoriteRestaurant: new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"])
+    favoriteRestaurant: resto2
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
